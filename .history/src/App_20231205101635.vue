6a8d916ed8d284cfeed454179cec55208b5784fa
裴 丽波 <tong_guan1024@163.com>
2023-12-05 11:15:59 +0800

    更新husky格式化

--- .history/src/App_20231205101635.vue
+++ .history/src/App_20231205101635.vue
@@ -3,11 +3,7 @@
 </template>
 
 <script lang="ts" setup>
-for(let i =0; i < 100;i++) 
-{
-  console.log(i);
 
-}
 </script>
 
 <style>
--- .history/src/App_20231205101718.vue
+++ .history/src/App_20231205101718.vue
@@ -6,7 +6,7 @@
 for(let i =0; i < 100;i++) 
 {
   console.log(i);
-
+  
 }
 </script>
 
