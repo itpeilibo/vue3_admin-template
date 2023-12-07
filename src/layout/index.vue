fb070c8b744fbb1b935a2e5a8a687939b0338a8b
裴 丽波 <tong_guan1024@163.com>
2023-12-07 14:29:48 +0800

    feat: 左侧菜单静态的搭建

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -2,6 +2,24 @@
   <div class="layout_container">
     <div class="layout_slider">
       <Logo></Logo>
+      <!--  展示菜单    -->
+      <!--  滚动组件    -->
+      <el-scrollbar class="scrollbar">
+        <!--  菜单组件    -->
+        <el-menu background-color="#001529" text-color="white">
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">数据大屏</el-menu-item>
+          <!-- 折叠菜单 -->
+          <el-sub-menu index="3">
+            <template #title>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="2-1">用户管理</el-menu-item>
+            <el-menu-item index="2-2">角色管理</el-menu-item>
+            <el-menu-item index="2-3">菜单管理</el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+      </el-scrollbar>
     </div>
     <!-- 顶部导航 -->
     <div class="layout_tabbar">222</div>
@@ -25,6 +43,11 @@ import Logo from '@/layout/logo/index.vue'
     width: $base-menu-width;
     height: 100vh;
     background-color: $base-menu-background;
+
+    .scrollbar {
+      width: 100%;
+      height: calc(100vh - $base-menu-logo-height);
+    }
   }
 
   .layout_tabbar {
