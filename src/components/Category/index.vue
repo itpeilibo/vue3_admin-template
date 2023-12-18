45d3869ca3b45bf689c7bfbf56dda71bb5ad3ec0
裴 丽波 <tong_guan1024@163.com>
2023-12-18 17:13:54 +0800

    feat: 新增分类数据的ts类型

--- src/components/Category/index.vue
+++ src/components/Category/index.vue
@@ -2,12 +2,16 @@
   <el-card>
     <el-form inline>
       <el-form-item label="一级分类">
-        <el-select>
-          <el-option label="北京"></el-option>
-          <el-option label="深圳"></el-option>
-          <el-option label="广州"></el-option>
+        <el-select v-model="categoryStore.c1Id" @change="handler">
+          <el-option
+            v-for="item in categoryStore.c1Arr"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id"
+          ></el-option>
         </el-select>
       </el-form-item>
+
       <el-form-item label="二级分类">
         <el-select>
           <el-option label="北京"></el-option>
@@ -15,6 +19,7 @@
           <el-option label="广州"></el-option>
         </el-select>
       </el-form-item>
+
       <el-form-item label="三级分类">
         <el-select>
           <el-option label="北京"></el-option>
@@ -28,10 +33,24 @@
 
 <script setup lang="ts">
 import { onMounted } from 'vue'
-
-onMounted(async () => {
-  let categoryStore = await
+import useCategoryStore from '@/store/modules/category.ts'
+let categoryStore = useCategoryStore()
+console.log('当前', categoryStore.c1Id)
+onMounted(() => {
+  getC1()
 })
+
+//通知仓库获取一级分类的方法
+const getC1 = () => {
+  //通知分类仓库发请求获取一级分类的数据
+  categoryStore.getC1()
+}
+
+//此方法即为一级分类下拉菜单的change事件(选中值的时候会触发,保证一级分类ID有了)
+const handler = () => {
+  //通知仓库获取二级分类的数据
+  categoryStore.getC2()
+}
 </script>
 
 <style lang="" scoped></style>
--- src/store/modules/category.ts
+++ src/store/modules/category.ts
@@ -1,17 +1,37 @@
 //商品分类全局组件的小仓库
 import { defineStore } from 'pinia'
+import { reqC1 } from '@/api/product/attr'
+import { CategoryResponseData } from '@/api/product/attr/type.ts'
+import { CategoryState } from '@/store/modules/types/type.ts'
 
 const useCategoryStore = defineStore('Category', {
-  state: () => {
+  state: (): CategoryState => {
     return {
       //存储一级分类的数据
       c1Arr: [],
+      // c2Arr: [],
       //存储一级分类的ID
       c1Id: '',
     }
   },
   actions: {
     //获取一级分类的方法
-    getC1() {},
+    async getC1() {
+      //发请求获取一级分类的数据
+      const result: CategoryResponseData = await reqC1()
+      if (result.code === 200) {
+        this.c1Arr = result?.data
+      }
+    },
+
+    //获取二级分类的数据
+    // async getC2() {
+    //   const result: CategoryResponseData = await reqC2(this.c1Id)
+    //   if (result.code == 200) {
+    //     this.c2Arr = result.data
+    //   }
+    // },
   },
 })
+
+export default useCategoryStore
