f439b291c374cbe4515fc9c7a52dfbb4f5e41c49
裴 丽波 <tong_guan1024@163.com>
2023-12-21 15:49:08 +0800

    feat: SPU模块静态搭建

--- src/views/product/spu/index.vue
+++ src/views/product/spu/index.vue
@@ -1,7 +1,40 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div>我是spu</div>
+  <div>
+    <Category :scene="scene"></Category>
+    <el-card style="margin: 10px 0">
+      <el-button type="primary" size="default" icon="Plus">添加SPU</el-button>
+      <el-table border style="margin: 10px">
+        <el-table-column
+          label="序号"
+          type="index"
+          align="center"
+          width="80px"
+        ></el-table-column>
+        <el-table-column label="SPU名称"></el-table-column>
+        <el-table-column label="SPU描述"></el-table-column>
+        <el-table-column label="SPU操作"></el-table-column>
+      </el-table>
+
+      <el-pagination
+        v-model:current-page="pageNo"
+        v-model:page-size="pageSize"
+        :page-sizes="[3, 5, 7, 9]"
+        :background="true"
+        layout=" prev, pager, next, jumper,->, sizes,total"
+        :total="400"
+      />
+    </el-card>
+  </div>
 </template>
+<script setup lang="ts">
+import Category from '@/components/Category/index.vue'
+import { ref } from 'vue'
+//场景的数据
+let scene = ref<number>(0)
+//分页器默认页码
+let pageNo = ref<number>(1)
+//每一页展示几条数据
+let pageSize = ref<number>(3)
+</script>
 
 <style scoped></style>
